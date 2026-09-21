fa066164994cf29cedbc1c531e276381a98338da
Francisco Torres <francisco.torres@fdltech.cl>
2026-09-21 19:26:41 -0300

    Implement syscalls in systemcalls.c

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,10 @@
 #include "systemcalls.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h> 
+#include <fcntl.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -16,8 +22,11 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
+    int ret;
 
-    return true;
+    ret = system(cmd);
+
+    return (ret != -1);
 }
 
 /**
@@ -47,7 +56,7 @@ bool do_exec(int count, ...)
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
+    //command[count] = command[count];
 
 /*
  * TODO:
@@ -57,7 +66,30 @@ bool do_exec(int count, ...)
  *   (first argument to execv), and use the remaining arguments
  *   as second argument to the execv() command.
  *
-*/
+*/  
+    int pid = fork();
+
+    if (pid == -1) {
+        perror("fork");
+        return false;
+    }
+
+    if (!pid) {
+        int ret = execv(command[0], command);
+        if (ret == -1) {
+            perror("execv");
+            exit(1);
+        }
+    }
+
+    int status;
+
+    if (waitpid(pid, &status, 0) == -1) {
+        perror("waitpid");
+        return false;
+    } else if (WIFEXITED(status)) {
+        return (WEXITSTATUS(status) == 0);
+    }
 
     va_end(args);
 
@@ -82,7 +114,7 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
+    //command[count] = command[count];
 
 
 /*
@@ -92,6 +124,38 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+
+    int kidpid;
+    int fd = open(outputfile, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+    if (fd < 0) {
+        perror("open");
+        return false;
+    }
+    switch (kidpid = fork()) {
+        case -1:
+            perror("fork");
+            return false;
+        case 0:
+            if (dup2(fd, 1) < 0) {
+                perror("dup2");
+                exit(1);
+            }
+            close(fd);
+            execv(command[0], command);
+            perror("execvp");
+            exit(1);
+        default:
+            close(fd);
+    }
+
+    int status;
+
+    if (waitpid(kidpid, &status, 0) == -1) {
+        perror("waitpid");
+        return false;
+    } else if (WIFEXITED(status)) {
+        return (WEXITSTATUS(status) == 0);
+    }
 
     va_end(args);
 
